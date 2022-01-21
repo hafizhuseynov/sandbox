e30a5b1490348bd9e3f1ae0138153bc89a6e4750
hafizhuseynov <huseynoff.hafiz@gmail.com>
2022-01-21 02:15:42 -0800

    initial

---
+++ script/main.js
@@ -0,0 +1,35 @@
+const menuAll = document.querySelectorAll('.menu');
+
+function handleDropdown(elements){
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add('rise');
+    const parentElement = elements[i].parentElement;
+    let isMouseHover = false;
+
+    parentElement.addEventListener("mouseleave", function (event) {
+      isMouseHover = false;
+      setTimeout(() => {
+        if(isMouseHover === false ){
+          elements[i].classList.replace('rise','fade-out')
+          elements[i].style.display = 'none';
+          parentElement.style.color = 'black'
+        }
+      }, 80)
+      
+    }, false);
+  
+    parentElement.addEventListener("mouseover", function (event) {
+      isMouseHover = true
+      setTimeout(() => {
+        if(isMouseHover === true ){
+          elements[i].classList.replace('fade-out','rise');
+          elements[i].style.display = 'block';
+          parentElement.style.color = '#4285F4';
+        }
+      }, 50)
+    }, false);
+
+  };
+};
+
+handleDropdown(menuAll);
