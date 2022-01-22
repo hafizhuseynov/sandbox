8dccd346840868182d8f5704d367888accbd4ed2
hafizhuseynov <huseynoff.hafiz@gmail.com>
2022-01-22 10:17:46 -0800

    add: smart dropdown made:responsive

--- script/main.js
+++ script/main.js
@@ -1,34 +1,78 @@
 const menuAll = document.querySelectorAll('.menu');
+const navBarBtn = document.querySelector('.fa-bars');
+const closeNavBarBtn = document.querySelector('.fa-times');
+
+function setDropdowns(elements){
+  const nav = document.querySelector('nav');
+  const heightNav = nav.offsetHeight;
+  for (let i = 0; i < elements.length; i++) {
+    const parentElement = elements[i].parentElement;
+    if(parentElement.id === 'demos' || parentElement.id === 'documentation'){
+      elements[i].style.top = `${heightNav}px`;
+    } 
+  }
+}
+
+function closeMenu(menu){
+  menu.classList.replace('rise','fade-out')
+  menu.style.display = 'none';
+  menu.parentElement.style.color = '#252625';
+}
 
 function handleDropdown(elements){
+
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.add('rise');
     const parentElement = elements[i].parentElement;
     let isMouseHover = false;
+    let mouseLeaveMenu = false;
 
-    parentElement.addEventListener("mouseleave", function (event) {
-      isMouseHover = false;
+    parentElement.parentElement.addEventListener('mouseover', ()=>{
+      mouseLeaveMenu = false;
+    });
+
+    parentElement.parentElement.addEventListener('mouseleave', ()=>{
+      mouseLeaveMenu = true;
+    });
+
+    parentElement.addEventListener("mouseleave", ()=> {
       setTimeout(() => {
-        if(isMouseHover === false ){
-          elements[i].classList.replace('rise','fade-out')
-          elements[i].style.display = 'none';
-          parentElement.style.color = '#252625'
-        };
-      },80)
-      
+        //Don't wait when changing between neighbour dropdown labels
+        if(!mouseLeaveMenu){
+          closeMenu(elements[i]);
+        } 
+        
+        //Wait to go next dropdown menu
+        setTimeout(() => {
+          if(isMouseHover === false ){
+            closeMenu(elements[i]);
+          };
+        },300)
+      }, 0);
+
+      isMouseHover = false;
     }, false);
 
-    parentElement.addEventListener("mouseover", function (event) {
+    parentElement.addEventListener("mouseover", ()=> {
       isMouseHover = true
-      setTimeout(() => {
-        if(isMouseHover === true ){
-          elements[i].classList.replace('fade-out','rise');
-          elements[i].style.display = 'block';
-          parentElement.style.color = '#4285F4';
-        }
-      }, 80)
+      if(isMouseHover === true ){
+        elements[i].classList.replace('fade-out','rise');
+        elements[i].style.display = 'block';
+        parentElement.style.color = '#4285F4';
+      }
     }, false);
   };
 };
 
 handleDropdown(menuAll);
+setDropdowns(menuAll);
+
+navBarBtn.addEventListener('click', ()=>{
+  document.querySelector('.middle').style.left = 0;
+  closeNavBarBtn.style.display = 'block';
+})
+
+closeNavBarBtn.addEventListener('click', ()=>{
+  document.querySelector('.middle').style.left = '-100%'
+  closeNavBarBtn.style.display = 'none';
+})
